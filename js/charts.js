8047ecc1333a0a5d4593acfcaa2d832e1eeaac84
Adwaa <42051110+Adwaa@users.noreply.github.com>
2021-12-16 12:49:33 +0300

    add
    Signed-off-by: Adwaa <42051110+Adwaa@users.noreply.github.com>

--- js/charts.js
+++ js/charts.js
@@ -6,11 +6,13 @@ let myChart = new Chart(ordersChart, {
     data:{
         labels: ['Aug 1', 'Aug 3', 'Aug 5', 'Aug 7', 'Aug 9', 'Aug 11', 'Aug 13'],
         datasets: [{
-           // label: 'My First Dataset',
-            data: [1000, 4000, 2000, 3000, 500, 4000, 1500],
+            label: 'Orders',
+            data: [8000, 25000, 20000,32000, 30000,20000,40000],
             fill: true,
-            borderColor: '#da4167!important',
-            //tension: 0.1
+            backgroundColor: 'rgb(55, 49, 94,0.2)',
+            borderColor: 'rgb(255, 99, 132)',
+            borderWidth: 3,
+            tension: 0.1
           }]
     }, 
     options : {}
@@ -19,40 +21,22 @@ let myChart = new Chart(ordersChart, {
 
 // Bar Chart for Visitor
 var visitorChart = document.getElementById("myBarChart").getContext('2d');
-
 let myCharts = new Chart(visitorChart, {
     type: 'bar',
+
     data:{
-        labels: ['m', 'v', 's', 'e', 'n'],
-        datasets: [{
-            label: 'My First Dataset',
-            data: [100, 200, 350, 400, 600, 40],
-            backgroundColor:[
-              "rgba(255, 99, 132, 0.6)",
-             "rgba(255, 99, 132, 0.6)",
-             "rgba(255, 99, 132, 0.6)",
-              "rgba(255, 99, 132, 0.6)",
-              "rgba(255, 99, 132, 0.6)"
-            ],
-            //fill: false,
-            borderWidth:1,
-            borderColor: 'rgb(75, 192, 192)',
-            hoverBorderWidth: 1,
-            hoverBorderColor: '#000',
-           // tension: 0.1,
-           
-           options: {
-            scales: {
-              y: {
-                beginAtZero: true
-              }
-            }
-          },
-         // borderWidth: 1
-  }]
-          
-    }, 
-   //options : {}
+      labels: ['Aug 1', 'Aug 3', 'Aug 5', 'Aug 7', 'Aug 9', 'Aug 11', 'Aug 13'],
+      datasets: [{
+          label: 'Orders',
+          data: [8000, 25000, 20000,32000, 30000,20000,40000],
+          fill: true,
+          backgroundColor: 'rgb(55, 49, 94,0.2)',
+          borderColor: 'rgb(255, 99, 132)',
+          borderWidth: 3,
+          tension: 0.1
+        }]
+  }, 
+  options : {}
 });
 
 // Liner Chart for Cities
